b67586ca250a05ed6a0544449e79fb29d1fd0714
NgBlaze <okolaaprecious@gmail.com>
2023-12-31 19:40:43 +0100

    test

--- Advanced_Ichimoku.py
+++ Advanced_Ichimoku.py
@@ -1,104 +1,223 @@
-from datetime import datetime, timedelta
-import talib.abstract as ta
-import pandas_ta as pta
-from freqtrade.persistence import Trade
+# --- Do not remove these libs ---
 from freqtrade.strategy.interface import IStrategy
 from pandas import DataFrame
-from freqtrade.strategy import DecimalParameter, IntParameter
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+import pandas as pd  # noqa
+pd.options.mode.chained_assignment = None  # default='warn'
+import technical.indicators as ftt
 from functools import reduce
-import warnings
+from datetime import datetime, timedelta
+from freqtrade.strategy import merge_informative_pair
+import numpy as np
+from freqtrade.strategy import stoploss_from_open
 
-warnings.simplefilter(action="ignore", category=RuntimeWarning)
 
+class ichiV1(IStrategy):
 
-class E0V1E(IStrategy):
+    # NOTE: settings as of the 25th july 21
+    # Buy hyperspace params:
+    buy_params = {
+        "buy_trend_above_senkou_level": 1,
+        "buy_trend_bullish_level": 6,
+        "buy_fan_magnitude_shift_value": 3,
+        "buy_min_fan_magnitude_gain": 1.002 # NOTE: Good value (Win% ~70%), alot of trades
+        #"buy_min_fan_magnitude_gain": 1.008 # NOTE: Very save value (Win% ~90%), only the biggest moves 1.008,
+    }
+
+    # Sell hyperspace params:
+    # NOTE: was 15m but kept bailing out in dryrun
+    sell_params = {
+        "sell_trend_indicator": "trend_close_2h",
+    }
+
+    # ROI table:
     minimal_roi = {
-        "0": 1.5,
-        "30": 0.5,
-        "50": 0.75,
-        "60": 1
-
+        "0": 0.059,
+        "10": 0.037,
+        "41": 0.012,
+        "114": 0
     }
+
+    # Stoploss:
+    stoploss = -0.275
+
+    # Optimal timeframe for the strategy
     timeframe = '5m'
-    process_only_new_candles = True
-    startup_candle_count = 120
-    order_types = {
-        'entry': 'market',
-        'exit': 'market',
-        'stoploss': 'market',
-        'stoploss_on_exchange': True,
-        'stoploss_on_exchange_interval': 60,
-        'stoploss_on_exchange_market_ratio': 0.99
-    }
-    stoploss = -0.27
-    use_custom_stoploss = False
 
-    is_optimize_32 = True
-    buy_rsi_fast_32 = IntParameter(20, 70, default=45, space='buy', optimize=is_optimize_32)
-    buy_rsi_32 = IntParameter(15, 50, default=35, space='buy', optimize=is_optimize_32)
-    buy_sma15_32 = DecimalParameter(0.900, 1, default=0.961, decimals=3, space='buy', optimize=is_optimize_32)
-    buy_cti_32 = DecimalParameter(-1, 0, default=-0.58, decimals=2, space='buy', optimize=is_optimize_32)
-    sell_fastx = IntParameter(50, 100, default=70, space='sell', optimize=True)
-    sell_loss_cci = IntParameter(low=0, high=600, default=148, space='sell', optimize=False)
-    sell_loss_cci_profit = DecimalParameter(-0.15, 0, default=-0.04, decimals=2, space='sell', optimize=False)
-    sell_cci = IntParameter(low=0, high=200, default=90, space='sell', optimize=False)
+    startup_candle_count = 96
+    process_only_new_candles = False
+
+    trailing_stop = False
+    #trailing_stop_positive = 0.002
+    #trailing_stop_positive_offset = 0.025
+    #trailing_only_offset_is_reached = True
+
+    use_sell_signal = True
+    sell_profit_only = False
+    ignore_roi_if_buy_signal = False
+
+    plot_config = {
+        'main_plot': {
+            # fill area between senkou_a and senkou_b
+            'senkou_a': {
+                'color': 'green', #optional
+                'fill_to': 'senkou_b',
+                'fill_label': 'Ichimoku Cloud', #optional
+                'fill_color': 'rgba(255,76,46,0.2)', #optional
+            },
+            # plot senkou_b, too. Not only the area to it.
+            'senkou_b': {},
+            'trend_close_5m': {'color': '#FF5733'},
+            'trend_close_15m': {'color': '#FF8333'},
+            'trend_close_30m': {'color': '#FFB533'},
+            'trend_close_1h': {'color': '#FFE633'},
+            'trend_close_2h': {'color': '#E3FF33'},
+            'trend_close_4h': {'color': '#C4FF33'},
+            'trend_close_6h': {'color': '#61FF33'},
+            'trend_close_8h': {'color': '#33FF7D'}
+        },
+        'subplots': {
+            'fan_magnitude': {
+                'fan_magnitude': {}
+            },
+            'fan_magnitude_gain': {
+                'fan_magnitude_gain': {}
+            }
+        }
+    }
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # buy_1 indicators
-        dataframe['sma_15'] = ta.SMA(dataframe, timeperiod=15)
-        dataframe['cti'] = pta.cti(dataframe["close"], length=20)
-        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
-        dataframe['rsi_fast'] = ta.RSI(dataframe, timeperiod=4)
-        dataframe['rsi_slow'] = ta.RSI(dataframe, timeperiod=20)
-        # profit sell indicators
-        stoch_fast = ta.STOCHF(dataframe, 5, 3, 0, 3, 0)
-        dataframe['fastk'] = stoch_fast['fastk']
 
-        dataframe['cci'] = ta.CCI(dataframe, timeperiod=20)
+        heikinashi = qtpylib.heikinashi(dataframe)
+        dataframe['open'] = heikinashi['open']
+        #dataframe['close'] = heikinashi['close']
+        dataframe['high'] = heikinashi['high']
+        dataframe['low'] = heikinashi['low']
+
+        dataframe['trend_close_5m'] = dataframe['close']
+        dataframe['trend_close_15m'] = ta.EMA(dataframe['close'], timeperiod=3)
+        dataframe['trend_close_30m'] = ta.EMA(dataframe['close'], timeperiod=6)
+        dataframe['trend_close_1h'] = ta.EMA(dataframe['close'], timeperiod=12)
+        dataframe['trend_close_2h'] = ta.EMA(dataframe['close'], timeperiod=24)
+        dataframe['trend_close_4h'] = ta.EMA(dataframe['close'], timeperiod=48)
+        dataframe['trend_close_6h'] = ta.EMA(dataframe['close'], timeperiod=72)
+        dataframe['trend_close_8h'] = ta.EMA(dataframe['close'], timeperiod=96)
+
+        dataframe['trend_open_5m'] = dataframe['open']
+        dataframe['trend_open_15m'] = ta.EMA(dataframe['open'], timeperiod=3)
+        dataframe['trend_open_30m'] = ta.EMA(dataframe['open'], timeperiod=6)
+        dataframe['trend_open_1h'] = ta.EMA(dataframe['open'], timeperiod=12)
+        dataframe['trend_open_2h'] = ta.EMA(dataframe['open'], timeperiod=24)
+        dataframe['trend_open_4h'] = ta.EMA(dataframe['open'], timeperiod=48)
+        dataframe['trend_open_6h'] = ta.EMA(dataframe['open'], timeperiod=72)
+        dataframe['trend_open_8h'] = ta.EMA(dataframe['open'], timeperiod=96)
+
+        dataframe['fan_magnitude'] = (dataframe['trend_close_1h'] / dataframe['trend_close_8h'])
+        dataframe['fan_magnitude_gain'] = dataframe['fan_magnitude'] / dataframe['fan_magnitude'].shift(1)
+
+        ichimoku = ftt.ichimoku(dataframe, conversion_line_period=20, base_line_periods=60, laggin_span=120, displacement=30)
+        dataframe['chikou_span'] = ichimoku['chikou_span']
+        dataframe['tenkan_sen'] = ichimoku['tenkan_sen']
+        dataframe['kijun_sen'] = ichimoku['kijun_sen']
+        dataframe['senkou_a'] = ichimoku['senkou_span_a']
+        dataframe['senkou_b'] = ichimoku['senkou_span_b']
+        dataframe['leading_senkou_span_a'] = ichimoku['leading_senkou_span_a']
+        dataframe['leading_senkou_span_b'] = ichimoku['leading_senkou_span_b']
+        dataframe['cloud_green'] = ichimoku['cloud_green']
+        dataframe['cloud_red'] = ichimoku['cloud_red']
+
+        dataframe['atr'] = ta.ATR(dataframe)
 
         return dataframe
 
-    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
         conditions = []
-        dataframe.loc[:, 'enter_tag'] = ''
-        buy_1 = (
-                (dataframe['rsi_slow'] < dataframe['rsi_slow'].shift(1)) &
-                (dataframe['rsi_fast'] < self.buy_rsi_fast_32.value) &
-                (dataframe['rsi'] > self.buy_rsi_32.value) &
-                (dataframe['close'] < dataframe['sma_15'] * self.buy_sma15_32.value) &
-                (dataframe['cti'] < self.buy_cti_32.value)
-        )
-        conditions.append(buy_1)
-        dataframe.loc[buy_1, 'enter_tag'] += 'buy_1'
+
+        # Trending market
+        if self.buy_params['buy_trend_above_senkou_level'] >= 1:
+            conditions.append(dataframe['trend_close_5m'] > dataframe['senkou_a'])
+            conditions.append(dataframe['trend_close_5m'] > dataframe['senkou_b'])
+
+        if self.buy_params['buy_trend_above_senkou_level'] >= 2:
+            conditions.append(dataframe['trend_close_15m'] > dataframe['senkou_a'])
+            conditions.append(dataframe['trend_close_15m'] > dataframe['senkou_b'])
+
+        if self.buy_params['buy_trend_above_senkou_level'] >= 3:
+            conditions.append(dataframe['trend_close_30m'] > dataframe['senkou_a'])
+            conditions.append(dataframe['trend_close_30m'] > dataframe['senkou_b'])
+
+        if self.buy_params['buy_trend_above_senkou_level'] >= 4:
+            conditions.append(dataframe['trend_close_1h'] > dataframe['senkou_a'])
+            conditions.append(dataframe['trend_close_1h'] > dataframe['senkou_b'])
+
+        if self.buy_params['buy_trend_above_senkou_level'] >= 5:
+            conditions.append(dataframe['trend_close_2h'] > dataframe['senkou_a'])
+            conditions.append(dataframe['trend_close_2h'] > dataframe['senkou_b'])
+
+        if self.buy_params['buy_trend_above_senkou_level'] >= 6:
+            conditions.append(dataframe['trend_close_4h'] > dataframe['senkou_a'])
+            conditions.append(dataframe['trend_close_4h'] > dataframe['senkou_b'])
+
+        if self.buy_params['buy_trend_above_senkou_level'] >= 7:
+            conditions.append(dataframe['trend_close_6h'] > dataframe['senkou_a'])
+            conditions.append(dataframe['trend_close_6h'] > dataframe['senkou_b'])
+
+        if self.buy_params['buy_trend_above_senkou_level'] >= 8:
+            conditions.append(dataframe['trend_close_8h'] > dataframe['senkou_a'])
+            conditions.append(dataframe['trend_close_8h'] > dataframe['senkou_b'])
+
+        # Trends bullish
+        if self.buy_params['buy_trend_bullish_level'] >= 1:
+            conditions.append(dataframe['trend_close_5m'] > dataframe['trend_open_5m'])
+
+        if self.buy_params['buy_trend_bullish_level'] >= 2:
+            conditions.append(dataframe['trend_close_15m'] > dataframe['trend_open_15m'])
+
+        if self.buy_params['buy_trend_bullish_level'] >= 3:
+            conditions.append(dataframe['trend_close_30m'] > dataframe['trend_open_30m'])
+
+        if self.buy_params['buy_trend_bullish_level'] >= 4:
+            conditions.append(dataframe['trend_close_1h'] > dataframe['trend_open_1h'])
+
+        if self.buy_params['buy_trend_bullish_level'] >= 5:
+            conditions.append(dataframe['trend_close_2h'] > dataframe['trend_open_2h'])
+
+        if self.buy_params['buy_trend_bullish_level'] >= 6:
+            conditions.append(dataframe['trend_close_4h'] > dataframe['trend_open_4h'])
+
+        if self.buy_params['buy_trend_bullish_level'] >= 7:
+            conditions.append(dataframe['trend_close_6h'] > dataframe['trend_open_6h'])
+
+        if self.buy_params['buy_trend_bullish_level'] >= 8:
+            conditions.append(dataframe['trend_close_8h'] > dataframe['trend_open_8h'])
+
+        # Trends magnitude
+        conditions.append(dataframe['fan_magnitude_gain'] >= self.buy_params['buy_min_fan_magnitude_gain'])
+        conditions.append(dataframe['fan_magnitude'] > 1)
+
+        for x in range(self.buy_params['buy_fan_magnitude_shift_value']):
+            conditions.append(dataframe['fan_magnitude'].shift(x+1) < dataframe['fan_magnitude'])
+
         if conditions:
             dataframe.loc[
-                reduce(lambda x, y: x | y, conditions),
-                'enter_long'] = 1
+                reduce(lambda x, y: x & y, conditions),
+                'buy'] = 1
+
         return dataframe
 
-    def custom_exit(self, pair: str, trade: 'Trade', current_time: 'datetime', current_rate: float,
-                    current_profit: float, **kwargs):
-        dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
-        current_candle = dataframe.iloc[-1].squeeze()
 
-        if current_profit > 0:
-            if current_candle["fastk"] > self.sell_fastx.value:
-                return "fastk_profit_sell"
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
-            if current_candle["cci"] > self.sell_cci.value:
-                return "cci_profit_sell"
+        conditions = []
 
-        if current_candle["high"] >= trade.open_rate:
-            if current_candle["cci"] > self.sell_cci.value:
-                return "cci_sell"
+        conditions.append(qtpylib.crossed_below(dataframe['trend_close_5m'], dataframe[self.sell_params['sell_trend_indicator']]))
 
-        if current_profit > self.sell_loss_cci_profit.value:
-            if current_candle["cci"] > self.sell_loss_cci.value:
-                return "cci_loss_sell"
+        if conditions:
+            dataframe.loc[
+                reduce(lambda x, y: x & y, conditions),
+                'sell'] = 1
 
-        return None
-
-    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe.loc[(), ['exit_long', 'exit_tag']] = (0, 'long_out')
         return dataframe
-    
